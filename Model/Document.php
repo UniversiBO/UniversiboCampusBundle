124d53324cbe262458bee5a1f3dafb90c7f5d4bc
Davide Bellettini <davide.bellettini@gmail.com>
2013-02-05 22:02:39 +0100

    Adding model stubs #4

---
+++ Model/Document.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @license MIT
+ * @copyright (c) 2013, Associazione UniversiBO
+ */
+namespace Universibo\Bundle\CampusBundle\Model;
+
+/**
+ * Represents a Document
+ *
+ * @author Davide Bellettini <davide.bellettini@gmail.com>
+ */
+class Document
+{
+    
+}
---
+++ Model/DocumentSet.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license MIT
+ * @copyright (c) 2013, Associazione UniversiBO
+ */
+namespace Universibo\Bundle\CampusBundle\Model;
+
+/**
+ * Represents a Document set
+ *
+ * @author Davide Bellettini <davide.bellettini@gmail.com>
+ */
+class DocumentSet
+{
+}
